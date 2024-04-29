7c8d131ddee5d6ab639b5b0bd2a5d9d1d5f18cfd
maragnes <marieangedesireeagnes166@gmail.com>
2024-04-29 12:43:54 +0000

    base de donnée

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import (
 	"fmt"
 	"forum/controllers"
+	"forum/initializers"
 	"log"
 	"net/http"
 )
 
 func main() {
+	_, err := initializers.InitDB()
+	if err != nil {
+		panic(err)
+	}
 	// Définir les gestionnaire
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources"))))
 	// Définir les autres routes
 	http.HandleFunc("/", controllers.IndexHandler)
 
 	// Démarrer le serveur HTTP sur le port 8080
-	fmt.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server running on port 8081")
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
